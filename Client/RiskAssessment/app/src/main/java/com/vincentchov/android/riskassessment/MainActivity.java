7f2b278cb61c5c6e1e8ab9ba58a3e76cf9f34e2d
Vincent Chov <vincentchov@gmail.com>
2017-04-05 22:50:16 -0400

    Use OkHttp to test the endpoint in the MainActivity

--- Client/RiskAssessment/app/src/main/java/com/vincentchov/android/riskassessment/MainActivity.java
+++ Client/RiskAssessment/app/src/main/java/com/vincentchov/android/riskassessment/MainActivity.java
@@ -2,6 +2,19 @@ package com.vincentchov.android.riskassessment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Info","Building the OkHttpClient");
+
+        try {
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request request = new Request.Builder()
+                                         .url("http://androidrisk.uconn.edu/report/foobar")
+                                         .build();
+
+            okHttpClient.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if(response.isSuccessful() == false){
+                        throw new IOException("Unexpected code: " + response);
+                    }
+
+                    String jsonData = response.body().string();
+                    JSONObject jsonObject;
+                    try {
+                        jsonObject = new JSONObject(jsonData);
+                        System.out.println(jsonObject.getJSONObject("score").keys());
+                        Log.i("JSON output", jsonObject.toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
